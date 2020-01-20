9c4b346497d26ad6eb8d4fdb70851333d4da3ca2
Mikael Hadler <mikaelhadler@gmail.com>
2020-01-20 15:46:42 -0300

    test(raindrops): Finish the first challenge and upload the second challenge
    Study

---
+++ two-fer/two_fer.py
@@ -0,0 +1,8 @@
+def two_fer(name):
+    if name == '':
+        return 'One for you, one for me.'
+    if name == 'Alice':
+        return 'One for Alice, one for me.'
+    if name == 'Bob':
+        return 'One for Bob, one for me.'
+    pass
